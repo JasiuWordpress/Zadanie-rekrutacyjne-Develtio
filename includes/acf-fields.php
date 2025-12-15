87809bdfd528d1b9c0a21c6ba100eccc09914f06
Jan <jan.w.pankowski@gmail.com>
2025-12-15 17:25:48 +0100

    Update acf-fields.php
    Poprawka

--- includes/acf-fields.php
+++ includes/acf-fields.php
@@ -1,42 +1,38 @@
 <?php
-
 defined( 'ABSPATH' ) || exit;
 
-
-if( function_exists('acf_add_local_field_group') ):
-
-acf_add_local_field_group(array(
-    'key' => 'events_group',
-    'title' => 'Pola wydarzeń',
-    'fields' => array (
-          array (
-            'key' => 'field_data_event',
-            'label' => 'Data wydarzenia',
-            'name' => 'data_event',
-            'type' => 'date_time_picker',
-        ),
-        array (
-            'key' => 'field_limit_event',
-            'label' => 'Limit uczestników',
-            'name' => 'limit_event',
-            'type' => 'number',
-        ),
-        array (
-            'key' => 'field_opis_event',
-            'label' => 'Opis/Szczegóły',
-            'name' => 'opis_event',
-            'type' => 'textarea',
-        )
-    ),
-    'location' => array (
-        array (
-            array (
-                'param' => 'post_type',
-                'operator' => '==',
-                'value' => 'event',
+add_action('acf/init', function() {
+    acf_add_local_field_group(array(
+        'key' => 'events_group',
+        'title' => 'Pola wydarzeń',
+        'fields' => array(
+            array(
+                'key' => 'field_data_event',
+                'label' => 'Data wydarzenia',
+                'name' => 'data_event',
+                'type' => 'date_time_picker',
+            ),
+            array(
+                'key' => 'field_limit_event',
+                'label' => 'Limit uczestników',
+                'name' => 'limit_event',
+                'type' => 'number',
+            ),
+            array(
+                'key' => 'field_opis_event',
+                'label' => 'Opis/Szczegóły',
+                'name' => 'opis_event',
+                'type' => 'textarea',
             ),
         ),
-    ),
-));
-
-endif;
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'event',
+                ),
+            ),
+        ),
+    ));
+});
